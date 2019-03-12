b38bc675ee1bc87bd645d8e5d3882384f9480939
Anis HAJRI <anis.hajri@gmail.com>
2019-03-12 08:34:36 +0100

    FIX Http Method

--- src/main/java/com/knoor/api/controller/HadithController.java
+++ src/main/java/com/knoor/api/controller/HadithController.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +34,7 @@ public class HadithController {
 	@Autowired
 	HadithService hadithService;
 
-	@PostMapping(path = "/duplicate")
+	@GetMapping(path = "/duplicate")
 	@ResponseBody
 	public ResponseEntity<List<DuplicateInfos>> searchDuplicate() throws RestException {
 
