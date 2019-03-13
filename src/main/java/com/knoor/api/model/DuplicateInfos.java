6b08481075623f2deb5a194148902c4ca68a9e02
Anis HAJRI <anis.hajri@gmail.com>
2019-03-14 00:05:52 +0100

    full duplicate

--- src/main/java/com/knoor/api/model/DuplicateInfos.java
+++ src/main/java/com/knoor/api/model/DuplicateInfos.java
@@ -12,7 +12,7 @@ public class DuplicateInfos implements Serializable {
 	 */
 	private static final long serialVersionUID = 2175511483562824267L;
 
-//	private String id;
+	private String id;
 	private List<ObjectId> uniqueIds;
 	private int total;
 
@@ -36,20 +36,20 @@ public class DuplicateInfos implements Serializable {
 		this.total = total;
 	}
 
-//	public String getId() {
-//		return id;
-//	}
-//
-//	public void setId(String id) {
-//		this.id = id;
-//	}
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	
 	
 	
 	@Override
 	public String toString() {
-		return "DuplicateInfos [id=" + null + ", uniqueIds=" + uniqueIds + ", total=" + total + "]";
+		return "DuplicateInfos [id=" + id + ", uniqueIds=" + uniqueIds + ", total=" + total + "]";
 	}
 
 
